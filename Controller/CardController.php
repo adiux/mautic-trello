6e6f1cd9c5ee7ab56bd81ba8f6c294ac7ee0918b
Adrian Schimpf <adrian@smolio.ch>
2020-04-27 07:37:34 +0200

    WIP

--- Controller/CardController.php
+++ Controller/CardController.php
@@ -9,17 +9,21 @@
 
 namespace MauticPlugin\Idea2TrelloBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use MauticPlugin\Idea2TrelloBundle\Openapi\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Validator\Constraints as Assert;
 use Mautic\CoreBundle\Controller\FormController;
+use FOS\RestBundle\Util\Codes;
+use JMS\Serializer\SerializationContext;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ServerException;
 
 use MauticPlugin\Idea2TrelloBundle\Openapi\Model\NewCard;
 use MauticPlugin\Idea2TrelloBundle\Form\NewCardType;
 
-class CardController extends FormController
+class CardController extends Controller
 {
     public function indexAction($page = 1)
     {
@@ -41,6 +45,12 @@ class CardController extends FormController
     {
         $logger = $this->get('monolog.logger.mautic');
         $request = $this->get('request_stack')->getCurrentRequest();
+        $server = $this->get('open_api_server.api.api_server');
+        print '<pre>';
+        print '<h1>server</h1>';
+        print_r($server);
+        print '</pre>';
+        exit;
 
         $logger->warning('got request with id', [$contactId]);
         // $logger->warning('request', );
@@ -60,11 +70,13 @@ class CardController extends FormController
             $newCard = $form->getData();
 
             $valid = $this->validateRequestData($newCard);
-            if ($valid !== true) {
+            if (true !== $valid) {
                 return $valid;
             }
             
             // ... perform some action, such as saving the task to the database
+            $card = $this->postTrelloCard($newCard);
+            $logger->warn('posted card')
 
             // return $this->redirectToRoute('task_success');
         }
@@ -80,6 +92,64 @@ class CardController extends FormController
         // return $this->render('Idea2TrelloBundle:Card:new.html.twig', [
         //     'form' => $form->createView(),
         // ]);
+    }
+
+    /**
+     * Post a card to Trello
+     * Return true or error message
+     *
+     * @param [type] $data
+     * @return void
+     */
+    protected function postTrelloCard( NewCard $card = null)
+    {
+        $config = $this->getConfig();
+        $postUrl    = $this->getPostUrl($card);
+        $headers = $this->getHeaders();
+
+        try {
+            $client   = new Client(['timeout' => 15]);
+            $response = $client->post(
+                $postUrl,
+                [
+                    'headers'     => $headers,
+                ]
+            );
+
+            return $this->parseResponse($response);
+        } catch (ServerException $exception) {
+            $this->parseResponse($exception->getResponse());
+        } catch (\Exception $exception) {
+             return $exception->getMessage();
+        }
+    }
+
+    protected function getConfig()
+    {
+        $config['authorization_key']    =   "9ef17425c93fae626ad969e282ddb409";
+        $config['authorization_token']  =   "eff37dda8691f4f9a96de5d4bf6283e42ebc3870a6fce6c181ebf94ce74303a6";
+        $config['base_url'] = "https://api.trello.com/1/cards";
+        
+        return $config;
+    }
+    protected function getPostUrl($data)
+    {
+        $config = $this->getConfig();
+        $params = array();
+        foreach ($data as $key => $value) {
+            $params[] = $key."=".urlencode($value);
+        }
+        $params[] = 'key='.$config['authorization_key'];
+        $params[] = 'token='.$config['authorization_token'];
+        $urlParams = \implode('&', $params);
+
+        return $config['base_url'].'?'.$urlParams;
+    }
+    protected function getHeaders()
+    {
+        return array(
+            // 'X-Forwarded-For' => $event->getSubmission()->getIpAddress()->getIpAddress(),
+        );
     }
 
     /**
@@ -101,7 +171,40 @@ class CardController extends FormController
         //     return $response;
         // }
         if (empty($newCard->getName())) {
-            return new Response('Name not set', 400);
+            return $this->badRequest('Name not set');
+            // return new Response('Name not set', 400);
+        }
+
+        return true;
+    }
+     /**
+     * @return bool|mixed
+     */
+    private function parseResponse(Response $response): bool
+    {
+        $body       = (string) $response->getBody();
+        $error      = false;
+
+        if ($json = json_decode($body, true)) {
+            $body = $json;
+        } elseif ($params = parse_str($body)) {
+            $body = $params;
+        }
+
+        if (is_array($body)) {
+            if (isset($body['error'])) {
+                $error = $body['error'];
+            } elseif (isset($body['errors'])) {
+                $error = implode(', ', $body['errors']);
+            }
+        }
+
+        if (!$error && 200 !== $response->getStatusCode()) {
+            $error = (string) $response->getBody();
+        }
+
+        if ($error) {
+            throw new \InvalidArgumentException('Could not create Trello card: '.$error);
         }
 
         return true;
